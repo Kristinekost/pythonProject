f67b75b442cfcbbdaebf46dd1ea4c7d2c127be8c
Kristinekost <kriskosta228@gmail.com>
2023-06-03 20:56:44 +0200

    add dot

--- years.py
+++ years.py
@@ -4,6 +4,6 @@ years_remaining = 90 - age_as_int
 days_remaining = years_remaining * 365
 weeks_remaining = years_remaining * 52
 months_remaining = years_remaining * 12
-print(f"You have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months")
+print(f"You have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months.")
 
 
