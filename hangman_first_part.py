8e02fd7d4ea50146541ffe255e17289d613b51ad
Kristinekost <kriskosta228@gmail.com>
2023-06-17 20:36:41 +0200

    second part

--- hangman_first_part.py
+++ hangman_first_part.py
@@ -1,9 +1,12 @@
 import random
 word_list = ["ardvark", "baabok", "camel"]
 chosen_word = random.choice(word_list)
+print(f'Pssst, the solution is {chosen_word}.')
+display = []
+for i in chosen_word:
+    display += "_"
 guess = input("Guess a letter: ").lower()
-for letter in chosen_word:
-    if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+for i in range(len(chosen_word)):
+    if guess == chosen_word[i]:
+        display[i] = guess
+print(display)
