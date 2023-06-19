1e705526fc50d94520a40a847ec9e5912174bb23
Kristinekost <kriskosta228@gmail.com>
2023-06-19 15:16:00 +0200

    second part

---
+++ Functions_with_input.py
@@ -0,0 +1,15 @@
+# def greet_with_name(name):
+#     print(f"Hi {name}")
+#     print(f"How do you do? {name}")
+#     print(f"Isn't the weather nice today? {name}")
+# greet_with_name("Aleksei")
+
+# def greet_with(name,location):
+#     print(f"Hi {name}")
+#     print(f"What is it like in {location} ?")
+# greet_with("Aleksei", "Berlin")
+
+def greet_with(name, location):
+    print(f" Hi {name}")
+    print(f"What is it like in {location}?")
+greet_with(location = "Berlin", name = "Aleksei")
