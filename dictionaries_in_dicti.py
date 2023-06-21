2a12863e03a9a4116b25983a2c1690871a537c0e
Kristinekost <kriskosta228@gmail.com>
2023-06-21 18:37:07 +0200

    key values

---
+++ dictionaries_in_dicti.py
@@ -0,0 +1,17 @@
+# travel_log = {
+#     "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "total_visits": 12},
+#     "Germany": ["Berlin", "Hamburg", "Stuttgart"]
+# }
+
+# Nesting dictionary in a list
+travel_log = [
+    {"country": "France",
+     "cities_visited": ["Paris", "Lille", "Dijon"],
+     "total_visits": 12
+     },
+    {"country": "Germany",
+     "cities_visited": ["Berlin", "Hamburg", "Stuttgart"],
+     "total_visits": 2
+     },
+]
+print(travel_log)
